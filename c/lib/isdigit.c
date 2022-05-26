ad385296a237d9c072a0f6a4a1d3000f37abadc9
satoru.kitaguchi <rule.the.fate.myfirststory@gmail.com>
2022-05-27 00:20:44 +0900

    implement <ctype.h> int isdigit(int c)

---
+++ c/lib/isdigit.c
@@ -0,0 +1,6 @@
+#include "lib.h"
+
+int __isdigit(int c)
+{
+    return ((c >= 48) && (c <= 57));
+}
--- c/lib/lib.h
+++ c/lib/lib.h
@@ -2,6 +2,7 @@
 #define LIB_H
 
 int __isalpha(int c);
+int __isdigit(int c);
 int __isprint(int c);
 int __islower(int c);
 int __isupper(int c);
