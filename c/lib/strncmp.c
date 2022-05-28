73240c28457032357928381ae61bf0f13115c20c
Satoru Kitaguchi <64164948+satorunooshie@users.noreply.github.com>
2022-05-29 03:30:08 +0900

    implement <string.h> char strncmp(const char *s1, const char *s2, size_t n) (#18)

--- c/lib/strncmp.c
+++ c/lib/strncmp.c
@@ -1,10 +1,10 @@
 #include "lib.h"
 
-int __strcmp(const char *s1, const char *s2)
+int __strncmp(const char *s1, const char *s2, size_t n)
 {
-    signed int res;
+    signed char res;
     res = 0;
-    while (1)
+    while (n--)
     {
         res = *s1 - *s2++;
         if (res != 0 || !*s1++)
