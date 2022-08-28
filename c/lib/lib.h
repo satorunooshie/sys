f7c83d7e1436b59e8b76e5bea17de3bdff0d860a
Satoru Kitaguchi <64164948+satorunooshie@users.noreply.github.com>
2022-08-28 23:39:23 +0900

    implement <string.h> char *strstr(const char *haystack, const char *needle) (#25)

--- c/lib/lib.h
+++ c/lib/lib.h
@@ -17,12 +17,13 @@ int __toupper(int c);
 
 // <string.h>
 char *__strcat(char *restrict s1, const char *restrict s2);
-int __strcmp(const char *s1, const char *s2);
 char *__strcpy(char *dst, const char *src);
 char *__strdup(const char *s1);
 char *__strncat(char *restrict s1, const char *restrict s2, size_t n);
-int __strncmp(const char *s1, const char *s2, size_t n);
 char *__strncpy(char *dst, const char *src, size_t len);
+char *__strstr(const char *haystack, const char *needle);
+int __strcmp(const char *s1, const char *s2);
+int __strncmp(const char *s1, const char *s2, size_t n);
 size_t __strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
 size_t __strlcpy(char *restrict dst, const char *restrict src, size_t dstsize);
 size_t __strlen(const char *s);
