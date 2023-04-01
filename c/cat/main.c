03bdbb86c400cc0bd7a4789385673f495b65469f
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2023-04-01 17:42:00 +0900

    read stdin

--- c/cat/main.c
+++ c/cat/main.c
@@ -1,46 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-#define BUFFER_SIZE 2048
-
-static void cat(const char *path);
-static void die(const char *s);
+static void cat(FILE *f);
 
 int main(int argc, char *argv[])
 {
     int i;
 
-    if (argc < 2) {
-        fprintf(stderr, "%s: file name not given\n", argv[0]);
-        exit(1);
+    if (argc == 1) {
+        cat(stdin);
+        exit(0);
     }
     for (i = 1; i < argc; i++) {
-        cat(argv[i]);
+        FILE *f;
+        f = fopen(argv[i], "r");
+        if (!f) {
+            perror(argv[i]);
+            exit(1);
+        }
+        cat(f);
+        fclose(f);
     }
     exit(0);
 }
 
-static void cat(const char *path) 
+static void cat(FILE *f)
 {
-    int fd;
-    unsigned char buf[BUFFER_SIZE];
-    int n;
+    int c;
 
-    fd = open(path, O_RDONLY);
-    if (fd < 0) die(path);
-    for (;;) {
-        n = read(fd, buf, sizeof buf);
-        if (n < 0) die(path);
-        if (n == 0) break;
-        if (write(STDOUT_FILENO, buf, n) < 0) die(path);
-    }
-    if (close(fd < 0)) die(path);
+    while ((c = fgetc(f)) != EOF) if (putchar(c) < 0) exit(1);
 }
 
-static void die(const char *s)
-{
-    perror(s);
-    exit(1);
-}
+//static void __cat(const char *path) 
+//{
+//    int fd;
+//    unsigned char buf[BUFFER_SIZE];
+//    int n;
+//
+//    fd = open(path, O_RDONLY);
+//    if (fd < 0) die(path);
+//    for (;;) {
+//        n = read(fd, buf, sizeof buf);
+//        if (n < 0) die(path);
+//        if (n == 0) break;
+//        if (write(STDOUT_FILENO, buf, n) < 0) die(path);
+//    }
+//    if (close(fd < 0)) die(path);
+//}
+
+//static void die(const char *s)
+//{
+//    perror(s);
+//    exit(1);
+//}
