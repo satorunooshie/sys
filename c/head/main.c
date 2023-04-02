362bfb66b354c9389a8bd0d4fa15013ab502ad53
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2023-04-02 17:52:50 +0900

    add options

--- c/head/main.c
+++ c/head/main.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
 static void head(FILE *f, long nlines);
+
+#define DEFAULT_N_LINES 10
+
+static struct option longopts[] = {
+    {"lines", required_argument, NULL, 'n'},
+    {"help", no_argument, NULL, 'h'},
+    {0, 0, 0, 0}
+};
 
 int main(int argc, char *argv[])
 {
-    long nlines;
+    int opt;
+    long nlines = DEFAULT_N_LINES;
 
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
-        exit(1);
+    while ((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+        switch (opt) {
+            case 'n':
+                if (optarg == NULL) {
+                    fprintf(stderr, "UsageError: %s [-n LINES] [FILE ...]\n", argv[0]);
+                    exit(1);
+                }
+                nlines = atol(optarg);
+                break;
+            case 'h':
+                fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+                exit(0);
+            case '?':
+                fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+                exit(1);
+        }
     }
-    nlines = atoi(argv[1]);
-    if (argc == 2) head(stdin, nlines);
+
+    if (optind == argc) head(stdin, nlines);
     else {
         int i;
-        for (i = 2; i < argc; i++) {
+        for (i = optind; i < argc; i++) {
             FILE *f;
             f = fopen(argv[i], "r");
             if (!f) {
