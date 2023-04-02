c9b8b3ebf0479763fcad389151d42398bafb7ed9
Satoru Kitaguchi <64164948+satorunooshie@users.noreply.github.com>
2023-04-02 18:27:36 +0900

    improve cat command (#33)

--- c/cat/main.c
+++ c/cat/main.c
@@ -1,33 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void cat(FILE *f);
+#define _GNU_SOURCE
+#include <getopt.h>
+
+static void cat(FILE *f, int escape);
+
+static struct option longopts[] = {
+    {"escape", no_argument, NULL, 'e'},
+    {"help", no_argument, NULL, 'h'},
+    {0, 0, 0, 0}
+};
 
 int main(int argc, char *argv[])
 {
-    int i;
+    int opt;
+    int escape = 0;
 
-    if (argc == 1) {
-        cat(stdin);
-        exit(0);
-    }
-    for (i = 1; i < argc; i++) {
-        FILE *f;
-        f = fopen(argv[i], "r");
-        if (!f) {
-            perror(argv[i]);
-            exit(1);
+    while ((opt = getopt_long(argc, argv, "e", longopts, NULL)) != -1) {
+        switch (opt) {
+            case 'e':
+                escape = 1;
+                break;
+            case 'h':
+                fprintf(stdout, "Usage: %s [-e] [FILE ...]\n", argv[0]);
+                exit(0);
+            case '?':
+                fprintf(stderr, "Usage: %s [-e] [FILE ...]\n", argv[0]);
+                exit(1);
         }
-        cat(f);
-        fclose(f);
+    }
+
+    argc -= optind;
+    argv += optind;
+    if (argc == 0) cat(stdin, escape);
+    else {
+        int i;
+        for (i = 0; i < argc; i++) {
+            FILE *f;
+            f = fopen(argv[i], "r");
+            if (!f) {
+                perror(argv[i]);
+                exit(1);
+            }
+            cat(f, escape);
+            fclose(f);
+        }
     }
     exit(0);
 }
 
-static void cat(FILE *f)
+static void cat(FILE *f, int escape)
 {
     int c;
 
+    if (escape) {
+        while ((c = fgetc(f)) != EOF) {
+            switch (c) {
+                case '\t':
+                    if (fputs("\\t", stdout) == EOF) exit(1);
+                    break;
+                case '\n':
+                    if (fputs("$\n", stdout) == EOF) exit(1);
+                    break;
+                default:
+                    if (putchar(c) < 0) exit(1);
+                    break;
+            }
+        }
+        return;
+    }
     while ((c = fgetc(f)) != EOF) if (putchar(c) < 0) exit(1);
 }
 
